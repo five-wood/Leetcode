5644315b126e914c3af71509a6c858526a2a4678
five-wood <1750153939@qq.com>
2019-11-10 17:25:44 +0800

    三数之和

---
+++ 15.三数之和.cs
@@ -0,0 +1,49 @@
+using System.Collections.Generic;
+public class Solution {
+    public IList<IList<int>> ThreeSum(int[] nums) {
+        List<int>sortList=new List<int>();
+        for(int i=0;i<nums.Length;i++)
+            sortList.Add(nums[i]);
+        sortList.Sort((a,b)=>{
+            if(a<b)
+                return -1;
+            else if(a==b)
+                return 0;
+            else
+                return 1;
+            }
+        );
+        IList<IList<int>> result=new List<IList<int>>();
+        for(int i=0;i<sortList.Count;i++)
+        {
+            if(i>0&&sortList[i-1]==sortList[i])
+                continue;
+            int first=sortList[i];
+            int Lindex=i+1;
+            int Rindex=sortList.Count-1;
+            while(Lindex<Rindex)
+            {
+                int sum=first+sortList[Lindex]+sortList[Rindex];
+                if(sum==0)
+                {
+                    IList<int>tmp=new List<int>();
+                    tmp.Add(first);
+                    tmp.Add(sortList[Lindex]);
+                    tmp.Add(sortList[Rindex]);
+                    result.Add(tmp);
+                    while(Rindex>Lindex&&sortList[Rindex]==sortList[--Rindex])
+                    ;
+                    while(Lindex<Rindex&&sortList[Lindex]==sortList[++Lindex])
+                    ;
+                }
+                else if (sum>0)
+                    Rindex--;
+                else
+                    Lindex++;
+            }
+        }
+        return result.ToList();
+    }
+}
+// @lc code=end
+
